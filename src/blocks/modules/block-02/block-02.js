9dd4f6e5d83094505015f00662b0f4700e4a5893
Alexander <ya.love.google@yandex.ru>
2019-08-07 18:09:57 +0700

    main page refresh

---
+++ src/blocks/modules/block-02/block-02.js
@@ -0,0 +1,13 @@
+modules.define('block-01', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
